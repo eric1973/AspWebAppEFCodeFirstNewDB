9e6c340494b272d7c0b87f2b5262c9e1a32976e7
erich brunner <erich.brunner@live.at>
2016-04-27 12:57:25 +0200

    initial version

---
+++ DataLayer.DbContext/Context/BooksContext.cs
@@ -0,0 +1,26 @@
+namespace DataLayer.DbContext.Context
+{
+    using Entities.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class BooksContext : DbContext
+    {
+        // Your context has been configured to use a 'BooksContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'DataLayer.DbContext.Context.BooksContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'BooksContext' 
+        // connection string in the application configuration file.
+        public BooksContext()
+            : base("name=BooksContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Book> MyEntities { get; set; }
+    }
+}
